daaeeaea297981b0857705d0083e783bca51b6c5
Andy Blackledge <a.blackledge@building-blocks.com>
2017-10-30 13:12:17 +0000

    Adding redux thunk so that we can use redux with ajax requests

--- app/reducers/rootReducer.js
+++ app/reducers/rootReducer.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import menuReducer from './menuReducer';
 
-const rootReducer = combineReducers({
-  menuIsIn: menuReducer,
+
+const appReducer = combineReducers({
+  menuIsIn: menuReducer
 });
+
+const rootReducer = (state, action) => {
+  return appReducer(state, action)
+}
 
 export default rootReducer;
