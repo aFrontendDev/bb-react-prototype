9be329c9782188271ae14e8b27e2915384b2fbcf
Andy Blackledge <a.blackledge@building-blocks.com>
2017-11-15 15:54:06 +0000

    Adding in reducer test

---
+++ test/menuReducer.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import renderer from 'react-test-renderer';
+import menuReducer from '../app/reducers/menuReducer';
+import { actionTypes } from '../app/actions/actionTypes';
+
+it('should return menuIn true', () => {
+  expect(
+    menuReducer(
+      false,
+      {
+        type: actionTypes.menuIn
+      }
+    )
+  ).toEqual(true);
+});
+
+
+it('should return menuIn false', () => {
+  expect(
+    menuReducer(
+      true,
+      {
+        type: actionTypes.menuIn
+      }
+    )
+  ).toEqual(false);
+});
